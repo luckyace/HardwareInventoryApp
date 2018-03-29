399b629ad806dbd477bc0c124ded40e26da62faa
luckyace <bulatowf@yandex.ru>
2018-03-30 00:03:45 +0300

    preparing a scan view

--- app/src/main/java/io/github/luckyace/hardwareinventoryapp/MainActivity.kt
+++ app/src/main/java/io/github/luckyace/hardwareinventoryapp/MainActivity.kt
@@ -1,12 +1,21 @@
 package io.github.luckyace.hardwareinventoryapp
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v4.app.ActivityOptionsCompat
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fab.setOnClickListener {
+            val intent = Intent(MainActivity@this, ScanActivity::class.java)
+            val options = ActivityOptionsCompat.makeSceneTransitionAnimation(MainActivity@this,
+                    fab, "fab_transition")
+            startActivity(intent, options.toBundle())
+        }
     }
 }
